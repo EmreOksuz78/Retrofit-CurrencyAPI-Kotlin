437f2505f94c1281a9602d59c0cc96b657abc7ef
EMRE ÖKSÜZ <emreoksuz43978@gmail.com>
2024-07-21 15:17:46 +0300

    Coroutines added

--- app/src/main/java/com/emreoksuz/retrofitrxjavakotlin/service/CryptoAPI.kt
+++ app/src/main/java/com/emreoksuz/retrofitrxjavakotlin/service/CryptoAPI.kt
@@ -2,11 +2,13 @@ package com.emreoksuz.retrofitrxjavakotlin.service
 
 import com.emreoksuz.retrofitrxjavakotlin.model.CryptoModel
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CryptoAPI {
-    
-    @GET("ENTER EXTEND")
-    fun getData():Call<List<CryptoModel>>
+
+    //@GET("ENTER EXTEND")
+    @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
+    suspend fun getData():Response<List<CryptoModel>>
 
 }
--- app/src/main/java/com/emreoksuz/retrofitrxjavakotlin/view/MainActivity.kt
+++ app/src/main/java/com/emreoksuz/retrofitrxjavakotlin/view/MainActivity.kt
@@ -13,18 +13,26 @@ import com.emreoksuz.retrofitrxjavakotlin.adapter.RecyclerViewAdapter
 import com.emreoksuz.retrofitrxjavakotlin.databinding.ActivityMainBinding
 import com.emreoksuz.retrofitrxjavakotlin.model.CryptoModel
 import com.emreoksuz.retrofitrxjavakotlin.service.CryptoAPI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener{
 
-    private val BASE_URL="ENTER BASE_URL"
+    //private val BASE_URL="ENTER BASE_URL"
+    private val BASE_URL="https://raw.githubusercontent.com/"
     private var cryptoModels : ArrayList<CryptoModel>? = null
     private lateinit var binding: ActivityMainBinding
     private var recyclerViewAdapter:RecyclerViewAdapter?=null
+    private var job : Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,43 +59,72 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.Listener{
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(CryptoAPI::class.java)
 
-        val service = retrofit.create(CryptoAPI::class.java)
-        val call = service.getData()
+        job = CoroutineScope(Dispatchers.IO).launch {
+            val response = retrofit.getData()
 
-        call.enqueue(object: Callback<List<CryptoModel>>{
-            override fun onResponse(
-                call: Call<List<CryptoModel>>,
-                response: Response<List<CryptoModel>>
-            ) {
+            withContext(Dispatchers.Main){
                 if (response.isSuccessful){
+
                     response.body()?.let {
                         cryptoModels = ArrayList(it)
-
                         cryptoModels?.let {
                             recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
                             binding.recyclerView.adapter=recyclerViewAdapter
                         }
-
-
-                        for (cryptoModel : CryptoModel in cryptoModels!!){
-                            println(cryptoModel.currency)
-                            println(cryptoModel.price)
-                        }
-
                     }
+
                 }
             }
 
-            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
-                t.printStackTrace()
-            }
+        }
 
-        })
+
+        /*
+                val service = retrofit.create(CryptoAPI::class.java)
+                val call = service.getData()
+
+                call.enqueue(object: Callback<List<CryptoModel>>{
+                    override fun onResponse(
+                        call: Call<List<CryptoModel>>,
+                        response: Response<List<CryptoModel>>
+                    ) {
+                        if (response.isSuccessful){
+                            response.body()?.let {
+                                cryptoModels = ArrayList(it)
+
+                                cryptoModels?.let {
+                                    recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
+                                    binding.recyclerView.adapter=recyclerViewAdapter
+                                }
+
+
+                                for (cryptoModel : CryptoModel in cryptoModels!!){
+                                    println(cryptoModel.currency)
+                                    println(cryptoModel.price)
+                                }
+
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+                        t.printStackTrace()
+                    }
+
+                })
+
+                 */
     }
 
     override fun onItemClick(cryptoModel: CryptoModel) {
-        Toast.makeText(this,"Clicked : ${cryptoModel.currency}",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Clicked : ${cryptoModel.currency}",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job?.cancel()
     }
 
 }
